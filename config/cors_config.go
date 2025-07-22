54c86e28e619b63501f824ec8210cecb0c41aa91
Danni4421 <endaelek1234@gmail.com>
2025-07-22 22:30:27 +0700

    refactor: adjust CORS configuration

--- config/cors_config.go
+++ config/cors_config.go
@@ -7,8 +7,11 @@ import (
 
 func NewCORSConfig() fiber.Handler {
 	return cors.New(cors.Config{
-		AllowOrigins:     "https://portal.lenteracendekia.id, https://trialportal.lenteracendekia.id, http://localhost:3000",
-		AllowHeaders:     "Origin, Content-Type, Accept",
+		AllowOrigins:     "https://portal.lenteracendekia.id,https://trialportal.lenteracendekia.id,http://localhost:3000",
+		AllowMethods:     "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+		AllowHeaders:     "Origin,Content-Type,Accept,Authorization,X-Requested-With",
 		AllowCredentials: true,
+		ExposeHeaders:    "Content-Length",
+		MaxAge:           86400,
 	})
 }
