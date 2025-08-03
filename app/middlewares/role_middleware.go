db6aedf88cc7b06c049a626ddcafc7f648d5e1ec
Danni4421 <endaelek1234@gmail.com>
2025-08-04 05:59:19 +0700

    refactor: adjust role middleware to have rest params to retrieve multiple roles

--- app/middlewares/role_middleware.go
+++ app/middlewares/role_middleware.go
@@ -6,17 +6,19 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func RoleMiddleware(role string) fiber.Handler {
+func RoleMiddleware(roles ...string) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		userRole := c.Locals("userRole")
-		fmt.Println("User role from context:", userRole, "Expected role:", role)
-		if userRole != role {
-			return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
-				"status":  "fail",
-				"message": "Forbidden",
-				"error":   "You do not have permission to access this resource",
-			})
+		fmt.Println("User role from context:", userRole, "Allowed roles:", roles)
+		for _, role := range roles {
+			if userRole == role {
+				return c.Next()
+			}
 		}
-		return c.Next()
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"status":  "fail",
+			"message": "Forbidden",
+			"error":   "You do not have permission to access this resource",
+		})
 	}
 }
